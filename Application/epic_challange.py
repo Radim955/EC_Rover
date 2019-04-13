f248f8e12669d445bc41aa30a84625aa9f788ca0
Roman Oravec <xoravec@fi.muni.cz>
2019-04-13 16:36:17 +0300

    added comments to code

--- Application/epic_challange.py
+++ Application/epic_challange.py
@@ -59,26 +59,32 @@ class Arena:
         obj_c_dist = distance(c_dist, obj_dist, angle)
         """https://math.stackexchange.com/questions/543961/determine-third-point-of-triangle-when-two-points-and-all-sides-are-known """
         """https://www.triangle-calculator.com/?what=vc"""
-        #These lines calculate correct coordinates only if vehicle is at (0, c_dist)
+        
+        #just making variable names easier to read and put into formulas
         c = AB = c_dist
         a = BC = obj_dist
         b = AC = obj_c_dist
-        
+
+        #These two lines calculate correct coordinates only if vehicle is at (0, c_dist)
         Cy = ((AB**2) + (AC**2) -(BC**2))/(2*AB)
         Cx = math.sqrt((AC**2)-(Cy**2))
 
         #Calculate rotation angle based on vehicle coordinates
+        #TODO
+        """not sure if alpha or theta is correct angle, I used solution from:
+    https://math.stackexchange.com/questions/158679/how-to-calculate-coordinates-of-third-point-in-a-triangle-2d-knowing-2-points
+    second answer."""
         theta = (self.vehicle_loc[0] - corn[0])/AB
         theta = math.degrees(math.asin(theta))
         
         alpha = (self.vehicle_loc[1] - corn[1])/AB
         aplha = math.degrees(math.acos(alpha))
         
-        print(theta, aplha)
         point = rotate_point((Cy, Cx), theta, (0,0)) 
         self.objects += [point]
+        #TODO: figure out solution for all corners (or any reference point), now it works only for (0,0)
         print("Coordinates of object", obj_name,"are",round(point[1],0),round(point[0],0))
-        return(Cy, Cx)
+        return point
 
     #obj takes coordinates of a known object as a touple
     #angle is the angle between the known object and a new object
@@ -91,6 +97,7 @@ class Arena:
     def find_route(self):
         #use TSP solution from the other code here? 
         return
+    
     def get_corner(self, corner):
         if corner == 1:
             c = (0,0)
@@ -128,6 +135,7 @@ def rotate_point(point, angle, center_point=(0, 0)):
     return new_point
 
 #PLAYGROUND
+""" get test inputs from https://www.triangle-calculator.com/?what=vc"""
 arena = Arena()
 arena.vehicle_loc = (22,33)
 #                       c           a     angle B
